a18710a1c6ce0b0e9e858e5fb8921949d0167234
diosanshu <diosanshu@gmail.com>
2023-12-19 00:14:00 +0530

    Implemented SPM Lib

---
+++ Sources/FindMyIP/FIndMyIPModel.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Haadhya on 19/12/23.
+//
+
+import Foundation
+public struct FIndMyIPModel: Codable {
+    let ip: String
+    let network: String
+    let version: String
+    let city: String
+    let region: String
+    let region_code: String
+    let country: String
+}
